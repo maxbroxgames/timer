c749c9cc21fd39ee6836ca889db478dc6cfa7a90
maxbroxgames <124490874+maxbroxgames@users.noreply.github.com>
2023-03-05 11:37:31 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+let timer = 0
+let counter = document.getElementById('counter')
+let timerId = '';
+
+function start() {
+    timerId = setInterval(() => {
+        timer++;
+        counter.innerText = timer;
+    }, 1000)
+}
+function reset() {
+    clearInterval(timerId)
+    timer = 0;
+    counter.innerText = time;
+}
